e5aff7dd9ae4fece3d4841c41821d0d00df5b753
marcinpgit <marcinp2012@gmail.com>
2018-05-30 00:27:20 +0200

    build it: adding summary component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import ExpenseList from './ExpenseList';
 import ExpanseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpensesSummary />
         <ExpanseListFilters />
         <ExpenseList />
     </div>
