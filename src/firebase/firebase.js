b335fcef601dd9d52916de9aa8c2b0a7d8131ba6
marcinpgit <marcinp2012@gmail.com>
2018-06-01 22:35:58 +0200

    removing data from database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -27,17 +27,12 @@ database.ref().set({
       console.log('This failed.', e)
   });
 
-//   database.ref().set('This is my data.');
+// database.ref('isSingle').set(null);
 
-// database.ref('age').set(23);
-
-// database.ref('location/city').set('Sopot');
-
-database.ref('attributes').set({
-    height: 180,
-    weight: 79
-}).then(() => {
-    console.log('Second set call worked.');
-}).catch((e) => {
-    console.log('Thing didnt work for the second error', e);
-});
+// database.ref('isSingle')
+//     .remove()
+//     .then(() => {
+//         console.log('Data was removed');
+//     }).catch(() => {
+//         console.log('Did note remove data', e);
+//     });
