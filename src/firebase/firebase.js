0a9ea01e517146d7ae66b85920fa2da105166416
marcinpgit <marcinp2012@gmail.com>
2018-06-01 23:25:27 +0200

    fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,29 +13,61 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Marcin',
-    age: 18,
-    stressLevel: 6,
-    job: {
-        title: 'Software developer',
-        company: 'Google'
-    },
-    location: {
-        city: 'Gdansk',
-        country: 'Poland'
-    }
-  }).then(() => {
-      console.log('Data is saved!')
-  }).catch((e) => {
-      console.log('This failed.', e)
-  });
-
-database.ref().update({
-    stressLevel: 9,
-    'job/company': 'Amazon',
-    'location/city': 'Seattle'
+database.ref().on('value', (snapshot) => {
+    const val = snapshot.val();
+    console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
 });
+
+// database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     }).catch((e) => {
+//         console.log('Error fetching data', e);
+//     });
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error with data fetching', e)
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(30);
+// }, 10500);
+
+// database.ref().set({
+//     name: 'Marcin',
+//     age: 18,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Gdansk',
+//         country: 'Poland'
+//     }
+//   }).then(() => {
+//       console.log('Data is saved!')
+//   }).catch((e) => {
+//       console.log('This failed.', e)
+//   });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
 
 // database.ref('isSingle').set(null);
 
