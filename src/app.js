1bddfd436faa4fe0a7f94a75d663dca4a746c46f
marcinpgit <marcinp2012@gmail.com>
2018-06-05 22:51:37 +0200

    auth reducer

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import './styles/styles.scss';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import { firebase } from './firebase/firebase';
 // import './playground/promises';
@@ -34,6 +34,8 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+        store.dispatch(login(user.uid));
+        console.log('uid', user.uid);
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
             if (history.location.pathname === '/') {
@@ -41,6 +43,7 @@ firebase.auth().onAuthStateChanged((user) => {
             }
         });
     } else {
+        store.dispatch(logout());
         renderApp();
         history.push('/');
     }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
+import authReducer from '../reducers/auth';
 
 // Store creation
 
@@ -13,6 +14,7 @@ export default () => {
         combineReducers({
             expenses: expensesReducer,
             filters: filtersReducer,
+            auth: authReducer
         }),
         composeEnhancers(applyMiddleware(thunk))
         // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
